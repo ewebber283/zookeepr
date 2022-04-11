acb3135ebe59915ffed34809d08c26e92367d9e5
Eric Webber <ewebber283@gmail.com>
2022-04-11 17:03:22 -0500

    added get routes for animals and single animal

--- server.js
+++ server.js
@@ -33,6 +33,12 @@ filterbyQuery = (query, animalsArray) => {
 
     return filteredResults;
 }
+
+findById = (id, animalsArray) => {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+
+    return result;
+}
 app.get('/api/animals', (req, res) => {
     let results = animals;
 
@@ -41,6 +47,16 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 })
+
+app.get('/api/animals/:id', (req,res) => {
+    const result = findById(req.params.id, animals);
+    if(result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`API now on port ${PORT}!`);
 })
